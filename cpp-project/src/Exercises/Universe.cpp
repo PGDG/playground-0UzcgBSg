41e29cdcd208bf95ddd1f1e9e6a2d096e8d014e9
pgdg <paul.godard3@gmail.com>
2018-03-27 08:51:53 +0200

    Test des Assert

--- cpp-project/src/Exercises/Universe.cpp
+++ cpp-project/src/Exercises/Universe.cpp
@@ -2,11 +2,7 @@
 #include "Universe.h"
 // }
 
-int Universe::CountAllStars(vector<int>& galaxies) {
-    int total_stars = 0;
-    for(int stars : galaxies) {
-        total_stars = stars; // fix me!
-    }
-    return total_stars;
-}
+int somme(int a, int b) {
 
+    return a+b;
+}
