83239b9f4e71883f055129af1a6bedc2538d4307
pgdg <paul.godard3@gmail.com>
2018-04-09 10:33:38 +0200

    Version simplifiée défintive ?

--- cpp-project/src/UniverseTest.cpp
+++ cpp-project/src/UniverseTest.cpp
@@ -20,20 +20,6 @@ void Success(bool success) {
     cout << "TECHIO> success " << (success ? "true" : "false") << endl;
 }
 
-// check if a string exists in a text file
-bool ExistsInFile(string str, string filename) {
-    ifstream file(filename);
-    string line;
-
-    while (getline(file, line)) {
-        if (line.find(str) != string::npos ) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 void AssertEquals(int expected, int found, string message) {
     if(expected != found) {
         ostringstream error;
@@ -44,34 +30,17 @@ void AssertEquals(int expected, int found, string message) {
 
 int main() {
     try {
-        vector<int> somme (2,3);
-        AssertEquals(5, somme(2,3), "Running somme(2, 3)...");
+        int somme (2,5);
+        AssertEquals(7, somme(2,5), "Running somme(2, 5)...");
 
-        vector<int> somme(0,-1);
-        AssertEquals(-1, somme(0,-1), "Running somme(0,-1)...");
+        int somme(3,-3);
+        AssertEquals(0, somme(3,-3), "Running somme(3,-3)...");
         Success(true);
-
-        if (ExistsInFile("accumulate", "src/Exercises/Universe.cpp")) {
-            Message("My personal Yoda, you are. 🙏", "* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ");
-            Message("My personal Yoda, you are. 🙏", "           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ");
-            Message("My personal Yoda, you are. 🙏", "__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .");
-            Message("My personal Yoda, you are. 🙏", "'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　");
-            Message("My personal Yoda, you are. 🙏", " /'.-c    　   * ●  ¸.　　°     ° 　¸.    ");
-            Message("My personal Yoda, you are. 🙏", " |  /T      　　°     ° 　¸.     ¸ .　　  ");
-            Message("My personal Yoda, you are. 🙏", "_)_/LI");
-        } else {
-            Message("Kudos 🌟", "Did you know that you can use the accumulate method to sum up a vector? Try it!");
-            Message("Kudos 🌟", " ");
-            Message("Kudos 🌟", "#include <numeric>");
-            Message("Kudos 🌟", " ");
-            Message("Kudos 🌟", "vector<int> galaxies {37, 3, 2};");
-            Message("Kudos 🌟", "int totalStars = accumulate(galaxies.begin(), galaxies.end(), 0); // 42");
-        }
 
     } catch (const exception& e)  {
         Success(false);
         Message("Oops! 🐞", e.what());
-        Message("Hint 💡", "Did you properly accumulate all stars into 'totalStars'? 🤔");
+        Message("Hint 💡", "Il faut faire la somme des deux opérandes passées en argument à la fonction ! ");
     }
 
   return 0;
