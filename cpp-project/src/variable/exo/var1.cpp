19147f1a2f3eaf8c4a8dcfd16fcc2ac399f52bc5
PGDG <37665095+PGDG@users.noreply.github.com>
2018-04-20 16:20:22 +0200

    Update var1.cpp

--- cpp-project/src/variable/exo/var1.cpp
+++ cpp-project/src/variable/exo/var1.cpp
@@ -1,3 +1,5 @@
+// { autofold
 static void exo(int& char_MAX, int& char_MIN, int& unsigned_char_MAX, int& unsigned_char_MIN, int& int_MAX, int& int_MIN, unsigned int& unsigned_int_MAX, unsigned int& unsigned_int_MIN, long& long_MAX, long& long_MIN, unsigned long& unsigned_long_MAX, unsigned long& unsigned_long_MIN){
+  // }
   //écrir ici
 }
